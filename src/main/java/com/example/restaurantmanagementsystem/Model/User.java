f171a4290864752262c70eaef8cfcb579e34189b
user <250148@newuu.uz>
2026-04-11 15:10:08 +0500

    Some changes

--- src/main/java/com/example/restaurantmanagementsystem/Model/User.java
+++ src/main/java/com/example/restaurantmanagementsystem/Model/User.java
@@ -3,4 +3,19 @@ package com.example.restaurantmanagementsystem.Model;
 
 public class User {
 
+    private String username;
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
 }
