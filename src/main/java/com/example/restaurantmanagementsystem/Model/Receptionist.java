40ad48dfb5bbf854748cefef0e2c7a01f50f0631
user <250148@newuu.uz>
2026-04-20 11:14:04 +0500

    Added reservation for receptionist

--- src/main/java/com/example/restaurantmanagementsystem/Model/Receptionist.java
+++ src/main/java/com/example/restaurantmanagementsystem/Model/Receptionist.java
@@ -1,7 +1,14 @@
 package com.example.restaurantmanagementsystem.Model;
 
-public class Receptionist extends Employee{
+import java.time.LocalDateTime;
+
+public class Receptionist extends Employee {
     Receptionist(String name, String email, String phone, int employeeID, String dateJoined) {
         super(name, email, phone, employeeID, dateJoined);
+    }
+
+    public boolean createReservation(LocalDateTime time, int peopleCount, String notes, Customer customer) {
+        Reservation reservation = new Reservation(time, peopleCount, notes, customer);
+        return true;
     }
 }
